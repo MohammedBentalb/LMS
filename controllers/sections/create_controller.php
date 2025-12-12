670e13c7b0a4cec2b1a7ab0e48756200e143c078
Mohammed Bentalb <mohammedbentalb0@gmail.com>
2025-12-13 00:58:45 +0100

    feat(php): realising the functionality needed to send data in order to create one or more sections for a spesiific course at ones

--- controllers/sections/create_controller.php
+++ controllers/sections/create_controller.php
@@ -1,1 +1,35 @@
 <?php
+
+    require_once('./models/sections_model.php');
+    require_once('./models/courses_model.php');
+    
+    
+    if($course_id === null) header("Location: index.php");
+
+    $foundCourse = getSingleCourse($course_id);
+    if(!count($foundCourse)) header("Location: index.php");
+
+    $urlArray = explode("/", $_SERVER['HTTP_REFERER']);
+    $prevLocation = (end($urlArray));
+    
+
+    $titles = $_POST['section-title'];
+    $positions = $_POST['section-position'];
+    $contents = $_POST['section-content'];
+    $good = false;
+    $length = min(count($titles), count($contents), count($positions));
+
+    for($i = 0; $i < $length; $i++){
+        try{
+            $good = insertSingleSection($course_id, $titles[$i], $contents[$i], $positions[$i]);
+        }catch(mysqli_sql_exception $e){
+            if($e->getCode() === 1062){
+                $sections = getCourseSections($course_id);
+
+                $lastPosition = end($sections)['position'] + 1;
+                header("Location: {$prevLocation}&last_position=$lastPosition");
+            }
+        }
+    }
+    
+    if($good) header("Location: index.php?v=coursess&action=detail&course_id=$course_id");
--- views/sections/section_form.php
+++ views/sections/section_form.php
@@ -13,6 +13,7 @@
     <h1 class="form-title max-content"><?= $sEditMode ? "Improve This Section" : "Build Your Course Sections" ?></h1>
     <p class="form-sub-title max-content"><?= $sEditMode ? "Refine the content and keep your course sharp" : "Break your course into clear, engaging parts" ?></p>
     <form action="<?= $sEditMode ? "?v=sections&action=edit&section_id=$section_id" : "?v=sections&action=create&course_id=$course_id" ?>" method="POST" class="course-form" enctype="multipart/form-data">
+        <?=  isset($_GET['last_position']) ? "<p class='error-field global-error' data-error-name='global'>position sent is taken, start from {$_GET['last_position']} or higher</p>" : null?>
         <div class="form-field">
             <label for="section-title">what should we call the section</label>
             <input id="section-title" name="section-title[]" type="text" placeholder="What should we calll your section?" value="<?= $sEditMode ? $section[0]['title'] : '' ?>">
