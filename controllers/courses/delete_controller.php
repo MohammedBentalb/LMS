6a3bc30170b14a5ac8b2254cb199a15e1bddb1c7
Mohammed Bentalb <mohammedbentalb0@gmail.com>
2025-12-14 09:52:19 +0100

    feat(php): deleting images of a course once it's deleted

--- controllers/courses/delete_controller.php
+++ controllers/courses/delete_controller.php
@@ -2,5 +2,14 @@
 
 require_once("./models/courses_model.php");
 
+// get the course image name, to unlink it before deleeting a course
+
+$foundCourse = getSingleCourse($course_id);
+
+if(empty($foundCourse)){
+    require_once('./views/error/error.php');
+    return;
+}
+unlink('./public/images/' . $foundCourse[0]['image']);
 deleteSingleCourse($course_id);
 header('Location: index.php');
